70bc9107dcfb6f60ba066996c207c77a0ae19f9d
XiYang6666 <1782356858@qq.com>
2024-04-26 22:53:47 +0800

    :sparkles: improve BufferStream

--- src/bufferStream.ts
+++ src/bufferStream.ts
@@ -16,47 +16,59 @@ export abstract class BaseBufferStream {
 }
 
 export class BufferInputStream extends BaseBufferStream {
+    bytesRead: number = 0;
     read(length: number): Buffer {
-        const value = this.innerBuffer.subarray(0, length);
-        this.innerBuffer = this.innerBuffer.subarray(length);
+        const value = this.innerBuffer.subarray(this.bytesRead, this.bytesRead + length);
+        this.bytesRead += length;
         return value;
     }
 
     get<T>(type: DataType<T>): T {
-        const [value, offset] = type.read(this.innerBuffer, 0);
-        this.innerBuffer = this.innerBuffer.subarray(offset);
+        const [value, offset] = type.read(this.innerBuffer, this.bytesRead);
+        this.bytesRead += offset;
         return value;
     }
 }
 
 export class BufferOutputStream extends BaseBufferStream {
+    bytesWrite: number = 0;
     write(buffer: Buffer) {
+        this.bytesWrite += buffer.length;
         this.innerBuffer = Buffer.concat([this.innerBuffer, buffer]);
     }
 
     put<T>(type: DataType<T>, value: T): void {
-        this.innerBuffer = Buffer.concat([this.innerBuffer, type.write(value)]);
+        const valueBuffer = type.write(value);
+        this.bytesWrite == valueBuffer.length;
+        this.innerBuffer = Buffer.concat([this.innerBuffer, valueBuffer]);
     }
 }
 
 export class BufferStream extends BaseBufferStream implements BufferInputStream, BufferOutputStream {
+    bytesRead: number = 0;
+    bytesWrite: number = 0;
     read(length: number): Buffer {
         const value = this.innerBuffer.subarray(0, length);
+        this.bytesRead += length;
         this.innerBuffer = this.innerBuffer.subarray(length);
         return value;
     }
 
     get<T>(type: DataType<T>): T {
         const [value, offset] = type.read(this.innerBuffer, 0);
+        this.bytesRead == offset;
         this.innerBuffer = this.innerBuffer.subarray(offset);
         return value;
     }
 
     write(buffer: Buffer) {
+        this.bytesWrite += buffer.length;
         this.innerBuffer = Buffer.concat([this.innerBuffer, buffer]);
     }
 
     put<T>(type: DataType<T>, value: T): void {
-        this.innerBuffer = Buffer.concat([this.innerBuffer, type.write(value)]);
+        const valueBuffer = type.write(value);
+        this.bytesWrite += valueBuffer.length;
+        this.innerBuffer = Buffer.concat([this.innerBuffer, valueBuffer]);
     }
 }
